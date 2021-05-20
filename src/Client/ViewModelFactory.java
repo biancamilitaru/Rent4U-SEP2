2381ad61a15c9b3ead7f5125fb98c76c8af94e4c
radovansanta <santa.radovan@gmail.com>
2021-05-20 10:59:56 +0200

    Created: - ModelFactory - ViewFactory - ViewHandler - ViewModelFactory

---
+++ src/Client/ViewModelFactory.java
@@ -0,0 +1,25 @@
+package Client;
+
+public class ViewModelFactory
+{
+  private static ViewModelFactory instance = new ViewModelFactory();
+
+  public static ViewModelFactory getInstance(){
+    return instance;
+  }
+
+  private LogInViewModel logInViewModel;
+
+  private ViewModelFactory() {
+  }
+
+  public LogInViewModel getLogInViewModel() {
+    if (logInViewModel == null)
+      logInViewModel = new LogInViewModel(ModelFactory.getInstance().getClientModel());
+    return logInViewModel;
+  }
+
+
+
+
+}
