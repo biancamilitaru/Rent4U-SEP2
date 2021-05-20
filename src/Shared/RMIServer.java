f520b00d79c9ee936611b7fa80cf35b95f1ec4b9
biancamilitaru <75315082+biancamilitaru@users.noreply.github.com>
2021-05-20 11:19:35 +0200

    Client implementation

--- src/Shared/RMIServer.java
+++ src/Shared/RMIServer.java
@@ -7,4 +7,6 @@ import java.rmi.RemoteException;
 public interface RMIServer extends Remote
 {
     void startServer() throws RemoteException, AlreadyBoundException;
+    void registerClient(ClientCallBack clientCallBack) throws RemoteException;
+    void unregisterClient(ClientCallBack clientCallBack) throws RemoteException;
 }
