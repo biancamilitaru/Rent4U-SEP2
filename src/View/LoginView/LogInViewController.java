5a2e22a28bf969bcd1b1af7dfafdc8c46481597b
peterblasko <peeterblahaa@gmail.com>
2021-05-20 11:33:38 +0200

    commit

--- src/View/LoginView/LogInViewController.java
+++ src/View/LoginView/LogInViewController.java
@@ -1,11 +1,26 @@
 package View.LoginView;
 
 import Client.ViewHandler;
+import ViewModel.LogInViewModel;
 import javafx.fxml.FXML;
+
+import java.io.IOException;
 
 public class LogInViewController
 {
-
   @FXML private javafx.scene.control.TextField passwordTextField;
+
   private ViewHandler viewHandler;
+  protected LogInViewModel logInViewModel;
+
+
+  public void init(LogInViewModel logInViewModel)
+  {
+    this.logInViewModel = logInViewModel;
+    this.viewHandler=viewHandler;
+
+    passwordTextField.textProperty().bindBidirectional(logInViewModel.passwordProperty());
+
+
+  }
 }
