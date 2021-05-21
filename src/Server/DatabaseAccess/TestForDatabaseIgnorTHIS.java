06114797158831e0f09badc46f5429b6b4435ec9
biancamilitaru <75315082+biancamilitaru@users.noreply.github.com>
2021-05-21 09:46:08 +0200

    Some database functionalities

--- src/Server/DatabaseAccess/TestForDatabaseIgnorTHIS.java
+++ src/Server/DatabaseAccess/TestForDatabaseIgnorTHIS.java
@@ -4,7 +4,7 @@ import Client.Model.Vehicle;
 
 import java.sql.SQLException;
 
-public class Test {
+public class TestForDatabaseIgnorTHIS {
     public static void main(String[] args) throws SQLException {
         ManageVehicles manageVehicles = new Rent4UDAO();
         Vehicle vehicle = new Vehicle("A78", 7, "type", "make",
