44181f03600e2f6c1dbdbd088f78be909d5ab8c0
Tolnai <tolnai.milan22@gmail.com>
2021-05-24 21:29:07 +0200

    View controller for: -Create Booking -Set Status

--- src/Client/Views/SetStatusViewController/SetStatusViewController.java
+++ src/Client/Views/SetStatusViewController/SetStatusViewController.java
@@ -5,6 +5,10 @@ import Client.Core.ViewModelFactory;
 import Client.ViewModel.MenuViewModel;
 import Client.ViewModel.SetStatusViewModel;
 import Client.Views.ViewController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.awt.*;
 
 public class SetStatusViewController implements ViewController
 {
@@ -12,11 +16,26 @@ public class SetStatusViewController implements ViewController
   private ViewHandler viewHandler;
   private SetStatusViewModel setStatusViewModel;
 
+  @FXML Button setStatusButton;
+  @FXML Button goToMenuButton;
+  @FXML TextField statusTextField;
+
+
   @Override public void init(ViewHandler viewHandler,
       ViewModelFactory viewModelFactory)
   {
     this.viewHandler = viewHandler;
     setStatusViewModel = viewModelFactory.getSetStatusViewModel();
+  }
+
+  public void onMenuButton(ActionEvent evt)
+  {
+    viewHandler.openMainMenu();
+  }
+
+  public void setSetStatusButton(ActionEvent evt)
+  {
+    setStatusViewModel.setStatus(,statusTextField.getText());
   }
 
 
