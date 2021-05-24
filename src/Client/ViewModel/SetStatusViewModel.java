4d528bb9d442fd948c42df6f70559f43ca955a78
peterblasko <peeterblahaa@gmail.com>
2021-05-24 09:34:28 +0200

    Created View model for Setting Status of the vehicle

---
+++ src/Client/ViewModel/SetStatusViewModel.java
@@ -0,0 +1,17 @@
+package Client.ViewModel;
+
+import Client.Model.ClientModel;
+import Client.Model.Vehicle;
+
+public class SetStatusViewModel
+{
+  private ClientModel userModel;
+
+  public SetStatusViewModel(ClientModel userModel){
+    this.userModel = userModel;
+
+  }
+
+
+}
+
