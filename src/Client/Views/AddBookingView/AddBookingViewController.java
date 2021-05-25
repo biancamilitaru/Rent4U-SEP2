34c7a57ad6d56f106a541508b51448bb5b4f79f3
radovansanta <santa.radovan@gmail.com>
2021-05-25 12:32:59 +0200

    Created: - AddBookingViewController - connection with VehicleCell

--- src/Client/Views/AddBookingView/AddBookingViewController.java
+++ src/Client/Views/AddBookingView/AddBookingViewController.java
@@ -2,17 +2,23 @@ package Client.Views.AddBookingView;
 
 import Client.Core.ViewHandler;
 import Client.Core.ViewModelFactory;
+import Client.Model.Vehicle;
 import Client.ViewModel.AddBookingViewModel;
+import Client.Views.AddBookingView.VehicleViewCell.VehicleListViewCell;
 import Client.Views.ViewController;
 import Server.Model.Date;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.rmi.RemoteException;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class AddBookingViewController implements ViewController {
 
@@ -24,12 +30,28 @@ public class AddBookingViewController implements ViewController {
   @FXML TextField startMinute;
   @FXML TextField endHour;
   @FXML TextField endMinute;
+  @FXML ListView listView;
+
+  public final ObservableList<Vehicle> vehiclesObservableList = FXCollections.observableArrayList();
 
   @Override
   public void init(ViewHandler viewHandler, ViewModelFactory viewModelFactory)
+      throws SQLException, RemoteException
   {
     this.viewHandler = viewHandler;
     viewModel = viewModelFactory.getAddBookingViewModel();
+    getVehicleData(viewModel.getVehicles());
+    listView.setItems(vehiclesObservableList);
+    listView.setCellFactory(studentListView -> new VehicleListViewCell());
+  }
+
+  public ObservableList<Vehicle> getVehicleData(
+      ArrayList<Vehicle> vehiclesArrayList)
+  {
+    for (int x = 0; x<vehiclesArrayList.size(); x++){
+      vehiclesObservableList.add(vehiclesArrayList.get(x));
+    }
+    return vehiclesObservableList;
   }
 
   public void onCreateBookingButton(ActionEvent evt) throws RemoteException
