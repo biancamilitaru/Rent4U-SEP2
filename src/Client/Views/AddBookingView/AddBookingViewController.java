4c865674dcc97a0cf9f880a938591052d65e0628
Tolnai <tolnai.milan22@gmail.com>
2021-05-25 11:30:08 +0200

    Adding booking view controller

--- src/Client/Views/AddBookingView/AddBookingViewController.java
+++ src/Client/Views/AddBookingView/AddBookingViewController.java
@@ -4,15 +4,26 @@ import Client.Core.ViewHandler;
 import Client.Core.ViewModelFactory;
 import Client.ViewModel.AddBookingViewModel;
 import Client.Views.ViewController;
+import Server.Model.Date;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
 
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.rmi.RemoteException;
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class AddBookingViewController implements ViewController {
 
   private ViewHandler viewHandler;
   private AddBookingViewModel viewModel;
+  @FXML DatePicker startDatePicker;
+  @FXML DatePicker endDatePicker;
+  @FXML TextField startHour;
+  @FXML TextField startMinute;
+  @FXML TextField endHour;
+  @FXML TextField endMinute;
 
   @Override
   public void init(ViewHandler viewHandler, ViewModelFactory viewModelFactory)
@@ -23,6 +34,16 @@ public class AddBookingViewController implements ViewController {
 
   public void onCreateBookingButton(ActionEvent evt) throws RemoteException
   {
-    viewModel.createBooking();
+    int startHour1 = Integer.parseInt(startHour.getText());
+    int endHour1 = Integer.parseInt(endHour.getText());
+    int startMinute1 = Integer.parseInt(startMinute.getText());
+    int endMinute1 = Integer.parseInt(endMinute.getText());
+
+    LocalDate date1 = startDatePicker.getValue();
+    LocalDate date2 = endDatePicker.getValue();
+
+    Date startDate1 = new Date(date1.getYear(), date1.getMonth().getValue(), date1.getDayOfMonth(), startHour1, startMinute1);
+    Date endDate1 = new Date(date2.getYear(), date2.getMonth().getValue(), date2.getDayOfMonth(), endHour1, endMinute1);
+
   }
 }
