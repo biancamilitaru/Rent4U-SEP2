0d394ac2ff6d3273f4371c5deb755f5e8e404751
radovansanta <santa.radovan@gmail.com>
2021-05-26 10:28:40 +0200

    Created: - BookingListViewCell - created - BookingViewCell.fxml - FXML - created - ListOfBookingsViewController - edited - ListOfBookingsView.fxml - FXML - created

--- src/Client/Views/ListOfBookingsView/ListOfBookingsViewController.java
+++ src/Client/Views/ListOfBookingsView/ListOfBookingsViewController.java
@@ -6,15 +6,14 @@ import Client.Model.Booking;
 import Client.Model.Vehicle;
 import Client.ViewModel.ListOfBookingsViewModel;
 import Client.ViewModel.ListOfVehiclesViewModel;
+import Client.Views.AddBookingView.VehicleViewCell.VehicleListViewCell;
+import Client.Views.ListOfBookingsView.BookingViewCell.BookingListViewCell;
 import Client.Views.ViewController;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableCell;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.util.Callback;
 
 import java.rmi.RemoteException;
@@ -26,9 +25,9 @@ public class ListOfBookingsViewController implements ViewController
   private ViewHandler viewHandler;
   private ListOfBookingsViewModel listOfBookingsViewModel;
 
-  @FXML TableView table = new TableView();
+  @FXML ListView<Booking> listView;
 
-  public final ObservableList<Booking> tableObservableList = FXCollections.observableArrayList();
+  public final ObservableList<Booking> bookingsObservableList = FXCollections.observableArrayList();
 
 
   @Override public void init(ViewHandler viewHandler,
@@ -36,9 +35,12 @@ public class ListOfBookingsViewController implements ViewController
   {
     this.viewHandler = viewHandler;
     this.listOfBookingsViewModel = viewModelFactory.getListOfBookingsViewModel();
-    addEditButton();
     getBookingData(listOfBookingsViewModel.getBookings());
-    table.setItems(tableObservableList);
+
+    listView.setItems(bookingsObservableList);
+    listView.setCellFactory(vehicleListView -> new BookingListViewCell());
+
+    listView.setFixedCellSize(73);
   }
 
   public void onAddBookingButton(ActionEvent evt)
@@ -56,56 +58,9 @@ public class ListOfBookingsViewController implements ViewController
       ArrayList<Booking> bookingsArrayList)
   {
     for (int x = 0; x<bookingsArrayList.size(); x++){
-      tableObservableList.add(bookingsArrayList.get(x));
+      bookingsObservableList.add(bookingsArrayList.get(x));
     }
-    return tableObservableList;
-  }
-
-
-
-  private void addEditButton() {
-    TableColumn<Booking, Void> colBtn = new TableColumn("Edit");
-
-    Callback<TableColumn<Booking, Void>, TableCell<Booking, Void>> cellFactory = new Callback<TableColumn<Booking, Void>, TableCell<Booking, Void>>() {
-      @Override
-      public TableCell<Booking, Void> call(final TableColumn<Booking, Void> param) {
-        final TableCell<Booking, Void> cell = new TableCell<Booking, Void>() {
-
-          private final Button btn = new Button("Edit");
-
-          {
-            btn.setOnAction((ActionEvent event) -> {
-              Booking data = getTableView().getItems().get(getIndex());
-              System.out.println("selectedData: " + data);
-              try {
-                viewHandler.openEditBooking(data);
-              } catch (SQLException throwables) {
-                throwables.printStackTrace();
-              } catch (RemoteException e) {
-                e.printStackTrace();
-              }
-            });
-          }
-
-          @Override
-          public void updateItem(Void item, boolean empty) {
-            super.updateItem(item, empty);
-            if (empty) {
-              setGraphic(null);
-            } else {
-              setGraphic(btn);
-            }
-          }
-        };
-        return cell;
-      }
-    };
-
-    colBtn.setCellFactory(cellFactory);
-    colBtn.setPrefWidth(82.5);
-
-    table.getColumns().add(colBtn);
-
+    return bookingsObservableList;
   }
 
 }
