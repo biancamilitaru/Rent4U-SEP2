4753caf9159b6cb114c39cc789e77bfac0e4774c
kyratolnai11 <266375@viauc.dk>
2021-05-27 13:53:49 +0200

    LogInMenu

---
+++ src/Client/Views/LogInForCustomerView/LogInForCustomerViewController.java
@@ -0,0 +1,17 @@
+package Client.Views.LogInForCustomerView;
+
+import Client.Core.ViewHandler;
+import Client.Core.ViewModelFactory;
+import Client.Views.ViewController;
+
+import java.rmi.RemoteException;
+import java.sql.SQLException;
+
+public class LogInForCustomerViewController implements ViewController
+{
+  @Override public void init(ViewHandler viewHandler,
+      ViewModelFactory viewModelFactory) throws SQLException, RemoteException
+  {
+
+  }
+}
