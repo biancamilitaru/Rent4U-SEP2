60e2f078e8ffa0907b54a100dcb7200171094da4
kyratolnai11 <266375@viauc.dk>
2021-06-01 13:22:28 +0200

    alerts to all edited classes

--- src/Client/Views/AddCustomerAccountView/AddCustomerAccountViewController.java
+++ src/Client/Views/AddCustomerAccountView/AddCustomerAccountViewController.java
@@ -107,6 +107,11 @@ public class AddCustomerAccountViewController implements ViewController
       alert.showAndWait();
       viewHandler.openListOfCustomers(manager);
     }
+    Alert alert = new Alert(Alert.AlertType.WARNING);
+    alert.setTitle("Invalid input");
+    alert.setContentText("Please enter a valid cpr number!");
+    alert.showAndWait();
+
 
   }
 
